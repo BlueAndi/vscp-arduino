576d9bf6bcb29928dddb55dd02fff15662b0175c
BlueAndi <web@blue-andi.de>
2015-03-07 15:24:44 +0100

    Example for the Seeed-Studio CAN BUS Shield added. http://www.seeedstudio.com/wiki/CAN-BUS_Shield

---
+++ VSCP/examples/Seeed-Studio-CAN_BUS_Shield/Seeed-Studio-CAN_BUS_Shield.ino
@@ -0,0 +1,152 @@
+/* The MIT License (MIT)
+ * 
+ * Copyright (c) 2014 - 2015, Andreas Merkle
+ * http://www.blue-andi.de
+ * vscp@blue-andi.de
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
+#include <VSCP.h>           // VSCP framework
+#include <SPI.h>            // SPI used for CAN controller communication
+#include <mcp_can.h>        // CAN controller driver
+#include <mcp_can_dfs.h>    // CAN controller driver defines
+
+// Create an instance of the VSCP framework
+VSCP  vscp;
+
+// Create an instance of the CAN controller driver
+MCP_CAN canCom(
+  10  // Set CS (chip select) pin
+);
+
+// Read a message from the transport layer, e.g. the CAN bus
+// If no message is received return false, otherwise true.
+bool transportRead(vscp_RxMessage * const rxMsg) {
+  
+  bool status   = false;
+  
+  if (CAN_MSGAVAIL == canCom.checkReceive())
+  {
+    unsigned long   canMsgId  = 0;
+    
+    if (CAN_OK == canCom.readMsgBufID(&canMsgId, &rxMsg->dataNum, rxMsg->data)) {
+    
+        rxMsg->vscpClass  = (uint16_t)((canMsgId >> 16) & 0x01ff);
+        rxMsg->vscpType   = (uint8_t)((canMsgId >> 8) & 0x00ff);
+        rxMsg->oAddr      = (uint8_t)((canMsgId >> 0) & 0x00ff);
+        rxMsg->hardCoded  = (uint8_t)((canMsgId >> 25) & 0x0001);
+        rxMsg->priority   = (VSCP_PRIORITY)((canMsgId >> 26) & 0x0007);
+            
+        status = true;
+    }
+  }
+  
+  return status;
+}
+
+// Write a message to the transport layer, e.g. the CAN bus
+// If it fails to send the message return false, otherwise true.
+bool transportWrite(vscp_TxMessage const * const txMsg) {
+  
+  bool          status    = false;
+  unsigned long canMsgId  = 0;
+  
+  canMsgId = (((uint32_t)txMsg->priority)  << 26) |
+             (((uint32_t)txMsg->hardCoded) << 25) |
+             (((uint32_t)txMsg->vscpClass) << 16) |
+             (((uint32_t)txMsg->vscpType)  <<  8) |
+             txMsg->oAddr;
+  
+  if (CAN_OK == canCom.sendMsgBuf(canMsgId, 1, txMsg->dataNum, 0, (unsigned char*)txMsg->data)) {
+  
+    status = true;
+  }
+  
+  return status;
+}
+
+void setup() {
+
+  unsigned char retry = 3;  // Max. number of retries for CAN controller initialization
+
+  do {
+    
+    // Initialize CAN controller with 125 kbit/s (VSCP default bitrate)
+    if (CAN_OK != canCom.begin(CAN_125KBPS)) {
+        // Try again
+        delay(100);
+        --retry;
+    }
+    // Successful initialized
+    else
+    {
+        retry = 0;
+    }
+    
+  }while(0 < retry);
+  
+  // Only CAN frames with 29-bit identifier shall be received
+  canCom.init_Mask(0, 1, 0x1fffffff);
+  canCom.init_Mask(1, 1, 0x1fffffff);
+  canCom.init_Filt(0, 0, 0x00);
+  canCom.init_Filt(1, 0, 0x00);
+  canCom.init_Filt(2, 0, 0x00);
+  canCom.init_Filt(3, 0, 0x00);
+  canCom.init_Filt(4, 0, 0x00);
+  canCom.init_Filt(5, 0, 0x00);
+ 
+  // Node GUID - Used to unique identify nodes
+  VSCPGuid  nodeGuid = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 };
+  
+  // Setup VSCP framework
+  vscp.setup(
+    13,             // Status lamp pin
+    14,             // Init button pin
+    nodeGuid,       // Node GUID,
+    255,            // Node zone (255 = all zones)
+    255,            // Node sub-zone (255 = all sub-zones)
+    transportRead,  // VSCP framework calls it to read a message
+    transportWrite  // VSCP framework calls it to write a message
+  );
+  
+}
+
+void loop() {
+  // Process the VSCP framework
+  vscp.process();
+
+  // Send and receive VSCP messages only in active state
+  if (true == vscp.isActive()) {
+  
+    vscp_RxMessage  rxMsg;  // Receive message
+    vscp_TxMessage  txMsg;  // Transmit message
+    
+    // Any VSCP message received?
+    if (true == vscp.read(rxMsg)) {
+    
+        // Implement your code here ...
+        
+    }
+    
+    // Send a VSCP message here ...
+    
+  }
+}
